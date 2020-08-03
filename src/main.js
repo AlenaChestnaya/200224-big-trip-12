a3d638469d8fa7fffeaacf02815f4ec827e72835
Alena <chestnaja_alena@mail.ru>
2020-08-03 17:02:51 +0300

    поправила нарезанные из верстки компоненты

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ const EVENT_COUNT = 3;
 
 const createSiteMenuTemplate = () => {
   return (
-    `<nav class="trip-controls__trip-tabs  trip-tabs">
+    `<h2 class="visually-hidden">Switch trip view</h2>
+    <nav class="trip-controls__trip-tabs  trip-tabs">
       <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
       <a class="trip-tabs__btn" href="#">Stats</a>
     </nav>`
@@ -13,7 +14,8 @@ const createSiteMenuTemplate = () => {
 
 const createFilterTemplate = () => {
   return (
-    `<form class="trip-filters" action="#" method="get">
+    `<h2 class="visually-hidden">Filter events</h2>
+    <form class="trip-filters" action="#" method="get">
       <div class="trip-filters__filter">
         <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
         <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
@@ -297,28 +299,19 @@ const createEventEditTemplate = () => {
   );
 };
 
-const createGeneralInfoTemplate = () => {
-  return (
-    `<section class="trip-main__trip-info  trip-info">
-    </section>`
-  );
-};
-
 const createTripInfoTemplate = () => {
   return (
-    `<div class="trip-info__main">
-      <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+    `<section class="trip-main__trip-info  trip-info">
+      <div class="trip-info__main">
+        <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
 
-      <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
-    </div>`
-  );
-};
+        <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+      </div>
 
-const createTripCostTemplate = () => {
-  return (
-    `<p class="trip-info__cost">
-      Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
-    </p>`
+      <p class="trip-info__cost">
+        Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+      </p>
+    </section>`
   );
 };
 
@@ -328,12 +321,7 @@ const render = (container, template, place) => {
 
 const tripHeaderElement = document.querySelector(`.trip-main`);
 
-render(tripHeaderElement, createGeneralInfoTemplate(), `afterbegin`);
-
-const tripInfoElement = tripHeaderElement.querySelector(`.trip-info`);
-
-render(tripInfoElement, createTripInfoTemplate(), `afterbegin`);
-render(tripInfoElement, createTripCostTemplate(), `beforeend`);
+render(tripHeaderElement, createTripInfoTemplate(), `afterbegin`);
 
 const siteNavigationElement = tripHeaderElement.querySelector(`.trip-controls`);
 
